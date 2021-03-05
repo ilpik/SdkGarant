c0928bf90b1d5e66ef7182eff7a3a66fc67ccc65
leneos <shakhov94@yandex.ru>
2021-03-06 00:29:02 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+        public static int Solution(int[] arr)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int[] rangeI = Enumerable.Range(i - arr[i], arr[i]*2+1).ToArray();
+                
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    int[] rangeJ = Enumerable.Range(j - arr[j], arr[j]*2+1).ToArray();
+
+                    if (rangeJ.Any(valJ => rangeI.Contains(valJ)))
+                    {
+                        answer++;
+                    }
+                }
+            }
+            Console.WriteLine(answer);
+
+            return answer;
+        }
