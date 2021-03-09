df3b3e9eb3ba2691c70ee69efd38833bb38df3a5
leneos <shakhov94@yandex.ru>
2021-03-09 18:36:34 +0300

    100% solution

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var arr = new[] { 1, 5, 2, 1, 4, 2147483647 ,1};
+            //var arr = new[] { 1, 5, 2, 1, 4, 0};
+            var arr = new[] { 1, 2147483647, 0 };
+
             Solution(arr);
         }
 
@@ -17,26 +19,67 @@ namespace ConsoleApp
         {
             int answer = 0;
 
+            long[] startPoints = new long[A.Length];
+            long[] endPoints = new long[A.Length];
+
             for (int i = 0; i < A.Length; i++)
             {
+                startPoints[i] =(long)i - (long)A[i];
+                endPoints[i] = (long)i + (long)A[i];
+            }
+            long[] start = startPoints.OrderBy(i => i).ToArray();
+            long[] end = endPoints.OrderBy(i => i).ToArray();
+            int opendisks = 1;
+            int j = 1;
 
-                for (int j = i + 1; j < A.Length; j++)
+            foreach (var endVal in end)
+            {
+                for (; j < start.Length;)
                 {
-
-                    if ((long)(i + A[i]) >= (long)(j - A[j]))
+                    if (endVal >= start[j])
                     {
-                        if (answer < 10000000)
-                        {
-                            answer++;
-                        }
-                        else
+                        j++;
+                        answer += opendisks;
+                        opendisks++;
+                        if (answer > 10000000)
                         {
                             return -1;
                         }
+                    }
+                    else
+                    {
+                        opendisks--;
+                        break;
                     }
                 }
             }
             return answer;
         }
+
+        //public static int Solution(int[] A)
+        //{
+        //    int answer = 0;
+
+        //    for (int i = 0; i < A.Length; i++)
+        //    {
+
+        //        for (int j = i + 1; j < A.Length; j++)
+        //        {
+
+        //            if ((long)(i + A[i]) >= (long)(j - A[j]))
+        //            {
+        //                if (answer < 10000000)
+        //                {
+        //                    answer++;
+        //                }
+        //                else
+        //                {
+        //                    return -1;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return answer;
+        //}
     }
 }
